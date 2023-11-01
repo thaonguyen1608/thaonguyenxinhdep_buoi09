b90e4fa93d8bb1c0ea1cef7cc9bf5dc0cdc24243
Huynh thao nguyen <nuyenml084@gmail.com>
2023-11-01 15:39:34 +0700

    add file Bai1

---
+++ bai01/bai01/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool KT_MaKhoa(string ma)
+        {
+            try
+            {
+                connsql.Open();
+                string selectString = "select count(*)from Khoa where MaKhoa ='" + ma + "'";
+                SqlCommand cmd=new SqlCommand                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+            }
+        }
+    }
+}
